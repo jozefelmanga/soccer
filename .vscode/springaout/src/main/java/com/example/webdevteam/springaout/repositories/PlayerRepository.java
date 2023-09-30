f11b2d58ccf45f61c530a48cc4022421035118a2
Youssef Ben Salem <101717020+jozefelmanga@users.noreply.github.com>
2023-09-30 16:21:44 +0100

    Delete .vscode directory

--- .vscode/springaout/src/main/java/com/example/webdevteam/springaout/repositories/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.webdevteam.springaout.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.webdevteam.springaout.models.PlayerModel;
-
-public interface PlayerRepository extends JpaRepository<PlayerModel, Long> {
-
-}
